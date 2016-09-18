bc9d904fce54c8d5a7044f83fe72e9fe155bbc38
NealRame <contact@nealrame.com>
2016-09-18 20:54:26 +0200

    log deployment

--- scripts/remote-deploy.sh
+++ scripts/remote-deploy.sh
@@ -3,16 +3,26 @@
 pushd $(dirname $0)
 
 COMMIT="$1"
+DATE=$(date +"%m.%d.%y-%H:%M:%S")
 
 # extract the package
 tar xzf package.tgz
-rm package.tgz
 mv sources "$COMMIT"
 
+# link to latest
 if [ -L latest ];
 then
 	unlink latest
 fi
 ln -s "$COMMIT" latest
+
+# update deployment log file
+cat >> deploy.log <<EOF
+$DATE $COMMIT
+EOF
+
+# clean
+rm package.tgz
+rm remote-deploy.sh
 
 popd
